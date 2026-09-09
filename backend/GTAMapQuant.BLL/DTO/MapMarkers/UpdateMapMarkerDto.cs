43316c51d10e93fcfc60576d1534fe066b9324a0
Mykyta <nikita.oslaev@gmail.com>
2026-09-09 20:49:57 +0300

    feat: add map marker DTOs

---
+++ backend/GTAMapQuant.BLL/DTO/MapMarkers/UpdateMapMarkerDto.cs
@@ -0,0 +1,12 @@
+using GTAMapQuant.DAL.Entities;
+
+namespace GTAMapQuant.BLL.DTO.MapMarkers;
+
+public class UpdateMapMarkerDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public MarkerCategory Category { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+}
